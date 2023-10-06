f5893631cc8c6d69e292712add71b90569c5d882
Richard Kelly <richardkll120@gmail.com>
2023-10-05 17:19:48 -0700

    Final Submission

--- PyPoll/Pypoll_file.py
+++ PyPoll/Pypoll_file.py
@@ -1,17 +1,22 @@
+# Import Dependencies
 import os
 import csv
-   
+
+# Extract data in the budget_data.csv into lists.
 def extract_data_into_lists():
 
+    # Locate and extract election_data.csv.
     election_csv = os.path.join("PyPoll", "Resources", "election_data.csv")
 
     # Open and read csv
     with open(election_csv) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=",")
         csv_header = next(csv_file)
+        # Test
         print(f"Election Results")
         print("-------------------------")
         
+        # Set lists to be filled.
         ballots = []
         candidates = []
         unique_name = []
@@ -25,6 +30,7 @@ def extract_data_into_lists():
         
     return candidates
 
+# Set function to count all of the ballots submitted in the election
 def print_and_get_total_votes(candidates):
     total_votes = len(candidates)
     print(f'Total Votes: {total_votes}')
@@ -33,35 +39,41 @@ def print_and_get_total_votes(candidates):
     return total_votes
 
 
-
+# Set function to calculate how many votes each candidate received. 
 def candidate_statistics(candidates, unique_candidates):
+    # Collect all votes submitted.
     count_total = []
+    # Set list to collect the percentage of the vote each candidate received.
+    percent_vote_list = []
+    # Loop through the three candidates in the unique_candidates list.
     for candidate in unique_candidates:
+        # count the number of votes submitted for each candidate in the
+        # candidates list.
         count = candidates.count(candidate)
+        # Calculate the percentage of the vote each candidate received rounding
+        # to 3 decimal points.
         percent_vote = round((count/total_votes)*100,3)
+        # Enter percent_vote into the percent_vote_list.
+        percent_vote_list.append(percent_vote)
+        # Test
         print(f'{candidate}: {percent_vote}% ({count})')
     print("----------------------")
-    
 
-#def find_winner(candidates, unique_candidates):
-   # for candidate in unique_candidates:
-    #    count = candidates.count(candidate)
-    #    if count ==
-
+# Call all functions that were defined
 candidates = extract_data_into_lists()
 total_votes = print_and_get_total_votes(candidates)
 unique_candidates = list(set(candidates))
 candidate_statistics(candidates, unique_candidates)
 
+# Set output path for with the election_results text file is to be placed.
+output_path = os.path.join("..", "output", "election_results.txt")
+
+# Define new text file to be written.
 with open('election_results.txt', 'w') as text:
     
-# Election Results
-# -------------------------
-# Total Votes: 369711
-# -------------------------
-# Charles Casper Stockham: 23.049% (85213)
-# Diana DeGette: 73.812% (272892)
-# Raymon Anthony Doane: 3.139% (11606)
-# -------------------------
-# Winner: Diana DeGette
-# -------------------------
+    # Write rows of text file.
+    text.write("Election Results\n")
+    text.write("--------------------------------\n")
+    text.write(f'Total Votes: {total_votes}\n')
+    text.write("--------------------------------\n")
+        
